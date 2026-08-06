347fdf556475a07fab3f5cb6763882cc915ade85
SkJishan04 <skjishan28012004@gmail.com>
2026-08-06 21:47:53 +0530

    edited config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
+from pathlib import Path
+
 MY_NAME = "Sk_Jishan"
+
+ROOT = Path(__file__).parent
+
+TEMPLATE_DIR = ROOT / "templates"
+
+OUTPUT_TEX = ROOT / "output" / "tex"
+OUTPUT_PDF = ROOT / "output" / "pdf"
 
 LATEX_COMPILER = "pdflatex"
